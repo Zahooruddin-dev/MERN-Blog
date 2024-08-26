a74484d64b6b78dc501ac4e97ac2c1a26416baff
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-08-26 21:19:43 +0500

    Works!! Fixed the mongoDB and also the HOME JSX file was rewrtien

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,85 +1,74 @@
 import { useContext, useEffect } from "react";
-import classes from "./styles.module.css";
 import { GlobalContext } from "../../context";
 import axios from "axios";
-import { useNavigate, useLocation } from "react-router-dom";
+import classes from "./styles.module.css";
+import { FaTrash, FaEdit } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
-export default function AddNewBlog() {
-  const { formData, setFormData, setIsEdit, isEdit } =
+export default function Home() {
+  const { blogList, setBlogList, pending, setPending } =
     useContext(GlobalContext);
   const navigate = useNavigate();
-  const location = useLocation();
 
-  async function handleSaveBlogToDatabase() {
-    const response = isEdit
-      ? await axios.put(
-          `http://localhost:5000/api/blogs/update/${location.state.getCurrentBlogItem._id}`,
-          {
-            title: formData.title,
-            description: formData.description,
-          }
-        )
-      : await axios.post("http://localhost:5000/api/blogs/add", {
-          title: formData.title,
-          description: formData.description,
-        });
-
+  async function fetchListOfBlogs() {
+    setPending(true);
+    const response = await axios.get("http://localhost:5000/api/blogs");
     const result = await response.data;
-    if (result) {
-      setIsEdit(false)
-      setFormData({
-        title: "",
-        description: "",
-      });
-      navigate("/");
+
+    if (result && result.blogList && result.blogList.length) {
+      setBlogList(result.blogList);
+      setPending(false);
+    } else {
+      setPending(false);
+      setBlogList([]);
     }
   }
 
-  useEffect(() => {
-    console.log(location);
-    if (location.state) {
-      const { getCurrentBlogItem } = location.state;
-      setIsEdit(true);
-      setFormData({
-        title: getCurrentBlogItem.title,
-        description: getCurrentBlogItem.description,
-      });
+  async function handleDeleteBlog(getCurrentId) {
+    const response = await axios.delete(
+      `http://localhost:5000/api/blogs/delete/${getCurrentId}`
+    );
+    const result = await response.data;
+
+    if (result?.message) {
+      fetchListOfBlogs();
+      // navigate(0)
     }
-  }, [location]);
+  }
+
+  function handleEdit(getCurrentBlogItem) {
+    console.log(getCurrentBlogItem);
+    navigate("/add-blog", { state: { getCurrentBlogItem } });
+  }
+
+  useEffect(() => {
+    fetchListOfBlogs();
+  }, []);
 
   return (
     <div className={classes.wrapper}>
-      <h1>{isEdit ? "Edit a Blog" : "Add a Blog"}</h1>
-      <div className={classes.formWrapper}>
-        <input
-          name="title"
-          placeholder="Enter Blog Title"
-          id="title"
-          type="text"
-          value={formData.title}
-          onChange={(e) =>
-            setFormData({
-              ...formData,
-              title: e.target.value,
-            })
-          }
-        />
-        <textarea
-          name="description"
-          placeholder="Enter Blog Description"
-          id="description"
-          value={formData.description}
-          onChange={(event) =>
-            setFormData({
-              ...formData,
-              description: event.target.value,
-            })
-          }
-        />
-        <button onClick={handleSaveBlogToDatabase}>
-          {isEdit ? "Edit Blog" : "Add Blog"}
-        </button>
-      </div>
+      <h1>Blog List</h1>
+      {pending ? (
+        <h1>Loading Blogs ! Please wait</h1>
+      ) : (
+        <div className={classes.blogList}>
+          {blogList && blogList.length ? (
+            blogList.map((blogItem) => (
+              <div key={blogItem._id}>
+                <p>{blogItem.title}</p>
+                <p>{blogItem.description}</p>
+                <FaEdit onClick={() => handleEdit(blogItem)} size={30} />
+                <FaTrash
+                  onClick={() => handleDeleteBlog(blogItem._id)}
+                  size={30}
+                />
+              </div>
+            ))
+          ) : (
+            <h3>No Blogs Added</h3>
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- server/db/index.js
+++ server/db/index.js
@@ -5,7 +5,7 @@ mongoose.set("strictQuery", false);
 
 mongoose
   .connect(
-    "mongodb+srv://mzkhan886:boombech@blog.q9xod.mongodb.net/"
+    "mongodb+srv://mzkhan886:boombech@cluster0.q9xod.mongodb.net/"
   )
   .then(() => console.log("Connected mongo db"))
   .catch((e) => console.log(e));
